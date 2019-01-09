b15839fb2c13b5005c943715b99e29cbfc298905
Mark Schrijver <ractoc@ractoc.com>
2019-01-09 01:11:21 +0100

    clean and initial Tree implementation
    Initial Tree implementation for the categories editor using the TreeBeard React module

--- src/main/js/components/CategoryList.js
+++ src/main/js/components/CategoryList.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import {Treebeard} from 'react-treebeard';
 
 import { getCategoryData } from "../actions/categoryActions";
 
 import CategoryListItem from "./CategoryListItem";
 
-import "./CategoryList.css";
+import style from "./CategoryList.css";
+import "./CategoryListItem.css";
 
 
 function mapStateToProps(state) {
@@ -14,13 +16,51 @@ function mapStateToProps(state) {
   };
 }
 
+  const decorators = {
+      Loading: (props) => {
+      console.log("Loading", props);
+          return (
+              <div style={props.style}>
+                  loading...
+              </div>
+          );
+      },
+      Toggle: (props) => {
+      console.log("Toggle", props);
+          return (
+              <div style={props.style}>
+                  <svg height={props.height} width={props.width}>
+                      // Vector Toggle Here
+                  </svg>
+              </div>
+          );
+      },
+      Header: ({node, style}) => {
+          return (
+              <div>
+                  {node.name}
+              </div>
+          );
+      },
+      Container: (props) => {
+        const {decorators, terminal, onClick, node} = props;
+          return (
+              <div className="category-list-item">
+                  <decorators.Header node={node}  style={style.header}/>
+              </div>
+          );
+      }
+  };
+
 class CategoryList extends Component {
   constructor() {
     super();
   }
+
   componentDidMount() {
     this.props.getCategoryData();
   }
+
   render() {
     if (this.props.categories.length === 0) {
         return (
@@ -29,11 +69,13 @@ class CategoryList extends Component {
           </div>
         );
     } else {
+    console.log(this.props.categories);
         return (
           <div className="category-list">
-            {this.props.categories.map(category => (
-              <CategoryListItem key={category.id} category={category} />
-            ))}
+            <Treebeard
+              data={this.props.categories}
+              decorators={decorators}
+            />
           </div>
         );
     }
--- src/main/js/pages/Categories.js
+++ src/main/js/pages/Categories.js
@@ -10,59 +10,11 @@ import "./Categories.css";
 class Categories extends Component {
   render() {
     const match = this.props.match;
-
     return (
       <div className="categories">
-        <CategoryList categories={this.props.data} />
-
-        <Route
-          path={`${match.url}/:id`}
-          render={props => this.renderCategoryDetails(props)}
-        />
-        <Route
-          exact
-          path={match.url}
-          render={() => (
-            <div id="categoryDetail">
-              <p>Please select a category.</p>
-            </div>
-          )}
-        />
+        <CategoryList />
       </div>
     );
-  }
-
-  renderCategoryDetails(routeProps) {
-    // get selected contact by the id that was passed in the URL
-    const selectedCategory = this.props.data.find(
-      c => c.id === routeProps.match.params.id
-    );
-
-    if (selectedCategory) {
-      return (
-        <div id="categoryDetail">
-          {this.props.editedCategory ? (
-            <CategoryEditForm
-              category={selectedCategory}
-              onCancel={this.props.cancelEdit}
-              onSave={this.props.saveCategory}
-            />
-          ) : (
-            <CategoryDetailView
-              category={selectedCategory}
-              onEdit={this.props.editCategory}
-              onDelete={this.props.deleteCategory}
-            />
-          )}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <p>Category not found</p>
-        </div>
-      );
-    }
   }
 }
 
